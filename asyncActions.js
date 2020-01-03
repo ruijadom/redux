6de7ed8c8447a61dcfa8ce429453bffaaca2d4aa
ruijadom <ruijadomingues@gmail.com>
2020-01-03 22:53:05 +0000

    async actions with redux-thunk

---
+++ asyncActions.js
@@ -0,0 +1,82 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+const thunkMiddleware = require("redux-thunk").default;
+const axios = require("axios");
+
+const initialState = {
+  loading: false,
+  users: [],
+  error: ""
+};
+
+const FETCH_USERS_REQUEST = "FETCH_USERS_REQUEST";
+const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
+const FETCH_USERS_FAILURE = "FETCH_USERS_FAILURE";
+
+const fecthUsersRequest = () => {
+  return {
+    type: FETCH_USERS_REQUEST
+  };
+};
+
+const fecthUsersSuccess = users => {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users
+  };
+};
+
+const fecthUsersFailure = error => {
+  return {
+    type: FETCH_USERS_FAILURE,
+    payload: error
+  };
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USERS_REQUEST:
+      return {
+        ...state,
+        loading: true
+      };
+
+    case FETCH_USERS_SUCCESS:
+      return {
+        loading: false,
+        users: action.payload,
+        error: ""
+      };
+    case FETCH_USERS_FAILURE:
+      return {
+        loading: false,
+        users: [],
+        error: action.payload
+      };
+  }
+};
+
+const fetchUsers = () => {
+  return function(dispatch) {
+    dispatch(fecthUsersRequest());
+
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        //console.log(response);
+        const users = response.data.map(user => user.id);
+        dispatch(fecthUsersSuccess(users));
+      })
+      .catch(error => {
+        //console.log(error);
+        dispatch(fecthUsersFailure(error.message));
+      });
+  };
+};
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+store.subscribe(() => {
+  console.log(store.getState());
+});
+store.dispatch(fetchUsers());
